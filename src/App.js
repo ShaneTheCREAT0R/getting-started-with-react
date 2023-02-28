bf7eff6ed00134ad8055aa2b427ca69b5677b163
Shane Pugmire <shaynstr@gmail.com>
2023-02-28 16:56:29 -0700

    made changes based on lesson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import "./App.css";
+import { useState } from "react";
+import SubscriberForm from "./SubscriberForm";
+import SubscriberList from "./SubscriberList";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [subscribers, setSubscribers] = useState([]);
+
+  const createSubscriber = (newSubscriber) =>
+    setSubscribers((currentSubscribers) => [
+      newSubscriber,
+      ...currentSubscribers,
+    ]);
+
+  const deleteSubscriber = (indexToDelete) =>
+    setSubscribers((currentSubscribers) =>
+      currentSubscribers.filter((post, index) => index !== indexToDelete)
+    );
+
+  return (
+    <>
+      <SubscriberForm createSubscriber={createSubscriber} />
+      <SubscriberList
+        subscribers={subscribers}
+        deleteSubscriber={deleteSubscriber}
+      />
+    </>
+  );
 }
 
 export default App;
-
-
---
+++ src/Greeting.js
@@ -0,0 +1,15 @@
+function Greeting ({language}) {
+    switch (language) {
+        case "es":
+          return <h1>Hola!</h1>;
+        case "fr":
+          return <h1>Bonjour!</h1>;
+        case "en":
+        default:
+          return <h1>Hello!</h1>;
+      }
+}
+
+
+
+export default Greeting
